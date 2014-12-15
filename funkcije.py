51e34376c03c022370f7fbb9d343f9deab33a113
ZarabecB11 <barbara.zarabec@student.fmf.uni-lj.si>
2014-12-15 11:40:55 +0100

    madel 1

--- funkcije.py
+++ funkcije.py
@@ -1,31 +1,42 @@
 
 import sqlite3
 datoteka_baze = "miniK.sqlite3"
+baza = sqlite3.connect(datoteka_baze, isolation_level=None)
 
 def poisciKnjigo(nasl="", avt="", le = "", zaloz = "", kljucna="", ):
     ''' Funkcija poisce vse knjige glede na iskane parametre. '''
     
-    c = baza.cursor()
-    c.execute("""SELECT *
-                 FROM knjige JOIN kljucnaBeseda ON knjige.id = kljucnaBeseda.knjiga
-                 WHERE naslov LIKE ?
-                 AND avtor LIKE ?
-                 AND leto LIKE ?
-                 AND zalozba LIKE ?
-                 AND kljucna LIKE ?""",
-              [nasl, avt, le, zaloz, kljucna])
-    poiskane = c.fetchall()
-    c.close()
+    with baza:
+        baza.execute("""SELECT *
+                     FROM knjige JOIN kljucnaBeseda ON knjige.id = kljucnaBeseda.knjiga
+                     WHERE naslov LIKE ?
+                     AND avtor LIKE ?
+                     AND leto LIKE ?
+                     AND zalozba LIKE ?
+                     AND kljucna LIKE ?""",
+        [nasl, avt, le, zaloz, kljucna])
+    poiskane = baza.fetchall()
     return poiskane
 
-##def izposodi(naslov):
-##    '''Funkcija izposodi knjigo. '''
+# v izdelavi 
+##def izposodi(ime, naslov):
+##    '''Funkcija izposodi knjigo z danim naslovom nekemu uporabniku. '''
 ##
-##    c = baza.cursor()
-##    c.execute("""SELECT na_voljo FROM izvod_knjige""")
-##    jeNaVoljo = c.fetchall():
+##    # preverimo, ce je knjiga sploh na voljo
+##    with baza:
+##        baza.execute("""SELECT na_voljo FROM izvod_knjige JOIN knjige ON knjige.id = izvod_knjige.knjiga
+#### imetabele.imestolpca
+##                 WHERE naslov.knjige LIKE ?""", [naslov])
+##    jeNaVoljo = baza.fetchall():
+##
+##    # izposodimo knjigo uporabniku s pravo id stevilko    
 ##    if jeNaVoljo == [1]:
-##        c.execute("""SELECT """)
+##        with baza:
+##            # baza.execute("""SELECT id FROM uporabnik WHERE kdo == id.uporabnik""")
+##            
+##            baza.execute("""INSERT INTO izposoja (izvod, uporabnik)
+##                     SELECT id.izvod_knjige FROM izvod_knjige,
+##                     SELECT id.uporabnik FROM uporabnik WHERE id.uporabnik LIKE ?""", [ime])
+##            baza.execute("""UPDATE izvod_knjige set na_voljo = 0 WHERE id.izvod_knjige = """)
 
 
-baza = sqlite3.connect(datoteka_baze, isolation_level=None)
