a050bd357496d34caa06f7de5fa7f5b250ea8696
ZarabecB11 <barbara.zarabec@student.fmf.uni-lj.si>
2014-12-22 11:48:10 +0100

    model3

--- funkcije.py
+++ funkcije.py
@@ -1,42 +1,63 @@
 
 import sqlite3
+from datetime import *
+
 datoteka_baze = "miniK.sqlite3"
 baza = sqlite3.connect(datoteka_baze, isolation_level=None)
 
-def poisciKnjigo(nasl="", avt="", le = "", zaloz = "", kljucna="", ):
-    ''' Funkcija poisce vse knjige glede na iskane parametre. '''
+def poisciKnjigo(naslov="", avtor="", leto="", zalozba="", kljucna_beseda=""):
+    '''Funkcija poisce vse knjige glede na iskane parametre.'''
     
     with baza:
         baza.execute("""SELECT *
-                     FROM knjige JOIN kljucnaBeseda ON knjige.id = kljucnaBeseda.knjiga
-                     WHERE naslov LIKE ?
-                     AND avtor LIKE ?
-                     AND leto LIKE ?
-                     AND zalozba LIKE ?
-                     AND kljucna LIKE ?""",
-        [nasl, avt, le, zaloz, kljucna])
-    poiskane = baza.fetchall()
-    return poiskane
+                     FROM knjige JOIN kljucneBesede ON knjige.id = kljucneBesede.knjiga
+                     WHERE knjige.naslov LIKE ?
+                     AND knjige.avtor LIKE ?
+                     AND knjige.leto LIKE ?
+                     AND knjige.zalozba LIKE ?
+                     AND kljucneBesede.beseda LIKE ?""",
+        [naslov, avtor, leto, zalozba, kljucna_beseda])
+    
+    return baza.fetchall()
+
+def poglejVse():
+    '''Funkcija vrne vse knjige, ki jih hrani knjižnica.'''
+    
+    with baza:
+        baza.execute("""SELECT * FROM knjige""")
+        return baza.fetchall()
 
 # v izdelavi 
-##def izposodi(ime, naslov):
-##    '''Funkcija izposodi knjigo z danim naslovom nekemu uporabniku. '''
-##
-##    # preverimo, ce je knjiga sploh na voljo
-##    with baza:
-##        baza.execute("""SELECT na_voljo FROM izvod_knjige JOIN knjige ON knjige.id = izvod_knjige.knjiga
-#### imetabele.imestolpca
-##                 WHERE naslov.knjige LIKE ?""", [naslov])
-##    jeNaVoljo = baza.fetchall():
-##
-##    # izposodimo knjigo uporabniku s pravo id stevilko    
-##    if jeNaVoljo == [1]:
-##        with baza:
-##            # baza.execute("""SELECT id FROM uporabnik WHERE kdo == id.uporabnik""")
-##            
-##            baza.execute("""INSERT INTO izposoja (izvod, uporabnik)
-##                     SELECT id.izvod_knjige FROM izvod_knjige,
-##                     SELECT id.uporabnik FROM uporabnik WHERE id.uporabnik LIKE ?""", [ime])
-##            baza.execute("""UPDATE izvod_knjige set na_voljo = 0 WHERE id.izvod_knjige = """)
+def izposodi(izvod, uporabnik):
+    '''Funkcija izposodi izvod dane knjige danemu uporabniku. '''
 
+    # preverimo, ce je knjiga sploh na voljo
+    with baza:
+        ## imetabele.imestolpca
+        baza.execute("""SELECT na_voljo FROM izvod_knjige WHERE id = ?""", [izvod])
+        jeNaVoljo = baza.fetchone()
+        if jeNaVoljo is None:
+            raise Exception("Knjiga ne obstaja.")
+        elif jeNaVoljo == 0:
+            raise Exception("Knjiga je izposojena.")
+        elif jeNaVoljo == 1:
+            baza.execute("""INSERT INTO izposoja (izvod, uporabnik) VALUES (?, ?)""", [izvod, uporabnik])
+            baza.execute("""UPDATE izvod_knjige SET na_voljo = 0 WHERE id = ?""", [izvod])
+            baza.execute("""UPDATE izposoje SET datum_izposoje = ?""", [date.today()])
 
+def zamudnine(datumIzposoje, datumVrnitve=date.today()):
+    '''Funkcija vrne zamudnino uporabnikov.'''
+    # zamudnina za en dan znaša 0.45 eur
+
+    kolikoCasa = datumVrnitve - datumIzposoje
+    if kolikoCasa < 20:
+        # ni zamudnine
+        pass
+    else:
+        # for zanja -> kolikoDniZamuja += zamudninaZaEnDan 
+        kolikoDni = datumIzposoje + 20
+
+def rezerviraj(izvod, uporabnik):
+    ''' Funkcija rezervira knjigo danemu uporabniku. '''
+
+    pass
