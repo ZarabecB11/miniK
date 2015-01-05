21e98ef59ba99be3d17f8d0064b33dc3e448c6bd
ZarabecB11 <barbara.zarabec@student.fmf.uni-lj.si>
2015-01-05 11:49:13 +0100

    Še enkrat model

--- funkcije.py
+++ funkcije.py
@@ -27,37 +27,63 @@ def poglejVse():
         baza.execute("""SELECT * FROM knjige""")
         return baza.fetchall()
 
-# v izdelavi 
 def izposodi(izvod, uporabnik):
     '''Funkcija izposodi izvod dane knjige danemu uporabniku. '''
+
+    # ali ima uporabnik kaj dolga?
+    with baza:
+        baza.execute("""SELECT dolg FROM uporabniki WHERE id = ?""", [uporabnik])
+        kolikoJe = baza.fetchone()
+        if kolikoJe != 0:
+            raise Exception("Najprej morate poravnati dolg!")
+    
+    
+    # kaj, ce ima rezervacijo uporabnik
 
     # preverimo, ce je knjiga sploh na voljo
     with baza:
         ## imetabele.imestolpca
         baza.execute("""SELECT na_voljo FROM izvod_knjige WHERE id = ?""", [izvod])
         jeNaVoljo = baza.fetchone()
-        if jeNaVoljo is None:
-            raise Exception("Knjiga ne obstaja.")
-        elif jeNaVoljo == 0:
-            raise Exception("Knjiga je izposojena.")
+        if jeNaVoljo != 1:
+            # knjiga je izposojena ali pa jo je rezerviral nekdo drug
+            raise Exception("Knjiga je izposojena ali rezervirana. ")
         elif jeNaVoljo == 1:
             baza.execute("""INSERT INTO izposoja (izvod, uporabnik) VALUES (?, ?)""", [izvod, uporabnik])
             baza.execute("""UPDATE izvod_knjige SET na_voljo = 0 WHERE id = ?""", [izvod])
-            baza.execute("""UPDATE izposoje SET datum_izposoje = ?""", [date.today()])
+            baza.execute("""UPDATE izposoje SET datum_izposoje = ?""", [date.today()]) # kdaj si je izposodil
+
 
 def zamudnine(datumIzposoje, datumVrnitve=date.today()):
     '''Funkcija vrne zamudnino uporabnikov.'''
-    # zamudnina za en dan znaša 0.45 eur
+    # zamudnina za en dan znaša 0.20 eur
 
-    kolikoCasa = datumVrnitve - datumIzposoje
-    if kolikoCasa < 20:
-        # ni zamudnine
-        pass
+    razlikaVDneh = (abs(datumVrnitve - datumIzposoje)).days
+    if razlikaVDneh < 20:
+        raise Exception("Zamudnine ni.")
     else:
-        # for zanja -> kolikoDniZamuja += zamudninaZaEnDan 
-        kolikoDni = datumIzposoje + 20
+        kolikoJe = (razlikaVDneh-20)*0.20
+        baza.execute("""INSERT INTO uporabnik (dolg) VALUES (?)""", [kolikoJe])
+
+def vrni(izvod):
+    # obracunaj tudi zamudnino
+    with baza:
+        # vrne knjigo
+        baza.execute("""UPDATE izvod_knjige SET na_voljo = 1 WHERE id = ?""", [izvod])
+        baza.execute("""SELECT dolg FROM uporabniki WHERE id = ?""", [uporabnik])
+
+ #####?????            
+    pass
+
+def placaj_dolg(uporabnik, znesek): # znesek??
+    # napiše koliko je dolga in nekje mora biti kasneje gumb "Plačaj"
+    
+    pass
 
 def rezerviraj(izvod, uporabnik):
     ''' Funkcija rezervira knjigo danemu uporabniku. '''
 
     pass
+
+# dodaj/uredi knjigo
+
